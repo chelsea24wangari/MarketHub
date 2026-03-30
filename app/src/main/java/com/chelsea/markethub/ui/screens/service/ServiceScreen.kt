40753394817574b2b80fd5f7cc185c1baff5f1b6
chelsea24wangari <chelsea14wangari@gmail.com>
2026-03-30 10:46:43 +0300

    new project

---
+++ app/src/main/java/com/chelsea/markethub/ui/screens/service/ServiceScreen.kt
@@ -0,0 +1,76 @@
+package com.chelsea.markethub.ui.screens.service
+
+import android.app.Service
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun ServiceScreen(){
+
+
+
+}
+
+@Preview
+@Composable
+fun ServiceScreenPreview(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
